287be10b75ea6dd28a5b567ed532c5c394c29758
telman03 <telmangadimov1@gmail.com>
2023-02-12 20:41:36 +0400

    Launcher edited

--- src/main/java/app/Launcher.java
+++ src/main/java/app/Launcher.java
@@ -3,7 +3,7 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Launcher
 @SpringBootApplication
 public class Launcher {
     public static void main(String[] args) {
