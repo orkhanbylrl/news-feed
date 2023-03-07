b9d1513a23cd31f7591fb7163ac3c511a9df4fe9
telman03 <telmangadimov1@gmail.com>
2023-03-07 19:21:56 +0400

    Added new news sources

--- src/main/java/app/controller/ArticlesController.java
+++ src/main/java/app/controller/ArticlesController.java
@@ -1,6 +1,8 @@
 package app.controller;
 
 import app.parser.data.TechCrunchParser;
+import app.parser.data.DroidLifeParser;
+import app.parser.data.TechStartupsParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,12 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class ArticlesController {
     private final TechCrunchParser techCrunchParser;
-
+    private final DroidLifeParser droidLifeParser;
+    private final TechStartupsParser techStartupsParser;
 
     @GetMapping("/news_feed")
     public String showDesignForm(Model model) {
         var a = techCrunchParser.getArticles();
+        var b = droidLifeParser.getArticles();
+        var c = techStartupsParser.getArticles();
         model.addAttribute("articleList", a);
+        model.addAttribute("articleList", b);
+        model.addAttribute("articleList", c);
         return "main-page";
     }
 
